a9a2818584ad73f05e6719f9c2ed4395d50f90f0
Foysal Hossain <foysalhossain772@gmail.com>
2024-02-20 19:47:52 +0600

    first functions added

--- script.js
+++ script.js
@@ -2,4 +2,19 @@
 let newTask = document.querySelector("#new-task");
 let form = document.querySelector("form");
 let todoUl = document.querySelector("#items");
-let completeUl = document.querySelector(".complete-list ul")
+let completeUl = document.querySelector(".complete-list ul");
+
+// functions
+const createTask = function (task) {
+    const listItem = document.createElement('li');
+    const checkBox = document.createElement('input');
+    const label = document.createElement('label');
+
+    label.innerText = task;
+    checkBox.type = 'checkbox';
+
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+
+    return listItem;
+}
